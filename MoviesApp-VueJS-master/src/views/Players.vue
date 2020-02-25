1ac7127f8f86664edf9210a65576c1744ab46f9a
Julian Mahecha <julian.mahecha@peiky.com>
2020-02-25 18:39:05 -0500

    triple filter

--- MoviesApp-VueJS-master/src/views/Players.vue
+++ MoviesApp-VueJS-master/src/views/Players.vue
@@ -4,7 +4,35 @@
       <b-row class="text-center">
         <!-- Iteración -->
         <b-container class="p-2">
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Name Search" v-model="filter"></b-form-input>
+          <b-form-input
+            size="sm"
+            class="mr-sm-2"
+            placeholder="Name Search"
+            v-model="filter_name"
+            v-if="fill == 1"
+          ></b-form-input>
+          <b-form-input
+            size="sm"
+            class="mr-sm-2"
+            placeholder="Team Search"
+            v-model="filter_team"
+            v-if="fill == 2"
+          ></b-form-input>
+          <b-form-input
+            size="sm"
+            class="mr-sm-2"
+            placeholder="Division Search"
+            v-model="filter_team"
+            v-if="fill == 3"
+          ></b-form-input>
+        </b-container>
+
+        <b-container>
+          <b-button-group>
+            <b-button variant="primary" @click="fill = 1, filter_team = '', filter_division = ''">Name</b-button>
+            <b-button variant="secondary" @click="fill = 2, filter_name = '', filter_division = ''">Team</b-button>
+            <b-button variant="primary" @click="fill = 3, filter_name = '', filter_team = ''">Division</b-button>
+          </b-button-group>
         </b-container>
         <b-container class="p-2">
           <b-button variant="btn btn-success" v-if="!show" @click="show = !show">Add Player</b-button>
@@ -12,13 +40,9 @@
         </b-container>
         <b-container class="p-2" v-if="show">
           <b-form @submit="addPlayer" v-if="form">
-            <b-form-group
-              id="input-group-1"
-              label="Player Name:"
-              label-for="input-1"
-            >
+            <b-form-group id="input-group-1" label="Player Name:" label-for="input-1">
               <b-form-input
-                id="input-1"
+                id="input-fn"
                 v-model="form.first_name"
                 type="text"
                 required
@@ -26,13 +50,9 @@
               ></b-form-input>
             </b-form-group>
 
-            <b-form-group
-              id="input-group-1"
-              label="Player Last Name:"
-              label-for="input-1"
-            >
+            <b-form-group id="input-group-1" label="Player Last Name:" label-for="input-1">
               <b-form-input
-                id="input-1"
+                id="input-ln"
                 v-model="form.last_name"
                 type="text"
                 required
@@ -41,11 +61,21 @@
             </b-form-group>
 
             <b-form-group id="input-group-2" label="Player Position:" label-for="input-2">
-              <b-form-input id="input-2" v-model="form.position" required placeholder="Enter Position"></b-form-input>
+              <b-form-input
+                id="input-2"
+                v-model="form.position"
+                required
+                placeholder="Enter Position"
+              ></b-form-input>
             </b-form-group>
 
             <b-form-group id="input-group-3" label="Player Team:" label-for="input-3">
-              <b-form-input id="input-3" v-model="form.team.full_name" required placeholder="Enter Team"></b-form-input>
+              <b-form-input
+                id="input-3"
+                v-model="form.team.full_name"
+                required
+                placeholder="Enter Team"
+              ></b-form-input>
             </b-form-group>
 
             <b-button type="submit" variant="primary">Submit</b-button>
@@ -58,6 +88,7 @@
             <b-card :title="dc.name" tag="article" style class="mb-2" id="dc-card">
               <b-card-text v-if="dc.height">Height: {{ dc.height }} ft</b-card-text>
               <b-card-text>Team: {{ dc.team }} {{ dc.team_ab }}</b-card-text>
+              <b-card-text>Conference: {{ dc.conference }}</b-card-text>
               <b-card-text>Division: {{ dc.division }}</b-card-text>
               <b-card-text></b-card-text>
             </b-card>
@@ -97,20 +128,22 @@ export default {
   data() {
     return {
       players: [],
-      filter: "",
+      filter_name: "",
+      filter_team: "",
       dc: {
         name: ""
       },
       link: "",
       show: false,
-      form: {first_name:"",
-            last_name:"",
-             position:"",
-            team:{
-              full_name:""
-            }
-             
-      }
+      form: {
+        first_name: "",
+        last_name: "",
+        position: "",
+        team: {
+          full_name: ""
+        }
+      },
+      fill: 1
     };
   },
   created() {
@@ -118,11 +151,29 @@ export default {
   },
   computed: {
     filteredPlayer() {
-      return this.filter == ""
+      if (this.filter_name == "" && this.filter_team == "") {
+        var filtered_players = this.players;
+      } else if (this.filter_name != "") {
+        var filtered_players = this.players.filter(item => {
+          return _.includes(item.first_name.toLowerCase(), this.filter_name);
+        });
+      } else if (this.filter_team != "") {
+        var filtered_players = this.players.filter(item => {
+          return _.includes(item.team.name.toLowerCase(), this.filter_team);
+        });
+      }else if (this.filter_team != "") {
+        var filtered_players = this.players.filter(item => {
+          return _.includes(item.team.division.toLowerCase(), this.filter_division);
+        });
+      }
+
+      return filtered_players;
+
+      /* return this.filter_name == ""
         ? this.players
         : this.players.filter(item => {
-            return _.includes(item.first_name.toLowerCase(), this.filter);
-          });
+            return _.includes(item.first_name.toLowerCase(), this.filter_name);
+          }); */
     }
   },
   methods: {
@@ -142,6 +193,7 @@ export default {
       this.dc.height = number.height_feet;
       this.dc.team = number.team.full_name;
       this.dc.team_ab = number.team.abbreviation;
+      this.dc.conference = number.team.conference;
       this.dc.division = number.team.division;
     },
     del(id) {
@@ -151,11 +203,19 @@ export default {
     persistence() {
       localStorage.setItem("players", JSON.stringify(this.players));
     },
-    addPlayer(evt){
+    addPlayer(evt) {
       evt.preventDefault();
       this.players.unshift(this.form);
-      persistence();
-      
+      this.persistence();
+      this.form = {
+        first_name: "",
+        last_name: "",
+        position: "",
+        team: {
+          full_name: ""
+        }
+      };
+      this.show = false;
     }
   }
 };
